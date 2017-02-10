c7b87645a68cc6b17a7e07c856102de30e551641
Lemuel Reyes <reyeslemuelj@gmail.com>
2017-02-10 01:16:16 -0800

    try different client id and token

--- js/test.js
+++ js/test.js
@@ -6,7 +6,7 @@ $('#button').on('click', function(event){
 
         $.ajax({
               method: 'GET',
-              url: 'https://api.instagram.com/v1/tags/'+ hashtag +'/media/recent?access_token=347099814.78d962b.04dfb03f22d1480e8e90bbc4c4502a65'
+              url: 'https://api.instagram.com/v1/tags/'+ hashtag +'/media/recent?access_token=347099814.ea93cc6.090dcae0f75f4781a04135c47713db52'
         })
         .done(function(instaData) {
            console.log(instaData);
